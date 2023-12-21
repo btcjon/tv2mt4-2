dc39c3938f7ad20ca375e7d74f64dc4d11464dc6
Jon Bennett <jonbennett@Jons-MacBook-Pro.local>
2023-12-21 16:07:20 -0500

    Add airtable_operations.py and test_app.py.

---
+++ airtable_operations.py
@@ -0,0 +1,56 @@
+# airtable_operations.py
+from business_rule_engine import RuleParser
+import yaml
+from airtable import Airtable
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
+AIRTABLE_TABLE_NAME = os.getenv('AIRTABLE_TABLE_NAME')
+AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
+
+class AirtableOperations:
+    def __init__(self):
+        self.airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, AIRTABLE_API_KEY)
+
+    def update_by_field(self, field_name, field_value, new_data):
+        self.airtable.update_by_field(field_name, field_value, new_data)
+
+airtable_operations = AirtableOperations()
+
+def update_airtable(symbol, keyword):
+    # Load rules from yaml file
+    with open("rules.yaml", 'r') as stream:
+        rules = yaml.safe_load(stream)
+
+    # Parse the rule
+    parser = RuleParser()
+    parser.parsestr(rules)
+
+    # Define the action functions
+    def update_resistance(value):
+        airtable_operations.update_by_field('Symbol', symbol, {'Resistance': value})
+
+    def update_support(value):
+        airtable_operations.update_by_field('Symbol', symbol, {'Support': value})
+
+    def update_td9buy(value):
+        airtable_operations.update_by_field('Symbol', symbol, {'TD9buy': value})
+
+    def update_td9sell(value):
+        airtable_operations.update_by_field('Symbol', symbol, {'TD9sell': value})
+
+    def update_trend(value):
+        airtable_operations.update_by_field('Symbol', symbol, {'Trend': value})
+
+    # Register the action functions
+    parser.register_function(update_resistance)
+    parser.register_function(update_support)
+    parser.register_function(update_td9buy)
+    parser.register_function(update_td9sell)
+    parser.register_function(update_trend)
+
+    # Execute the rules
+    parser.execute({"type": "update", "keyword": keyword})
---
+++ test_app.py
@@ -0,0 +1,50 @@
+import unittest
+from unittest.mock import patch, MagicMock
+import logging
+import sys
+import os
+import yaml
+from app import update_airtable
+from business_rule_engine import RuleParser
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+def update_airtable(symbol, keyword):
+    # Load rules from yaml file
+    with open("rules.yaml", 'r') as stream:
+        rules = yaml.safe_load(stream)
+
+    # Parse the rule
+    parser = RuleParser()
+    parser.parsestr(rules)
+
+    # Update the Airtable field if the rule condition is met
+    if parser.execute({"keyword": keyword}):
+        airtable_operations.update_by_field('Symbol', symbol, {'Trend': keyword})
+        logging.info(f"Updated Airtable: Set '{symbol}' to '{keyword}'")
+
+class TestUpdateAirtable(unittest.TestCase):
+    @patch('app.airtable_operations')
+    @patch('business_rule_engine.RuleParser')
+    @patch('app.yaml')
+    def test_update_airtable(self, mock_yaml, mock_parser, mock_airtable_operations):
+        # Mock the yaml.safe_load function to return a specific set of rules
+        mock_yaml.safe_load.return_value = """
+        - name: Test Rule
+          condition: keyword == "test"
+          action: update_airtable("Test", True)
+        """
+
+        # Mock the RuleParser.execute function to return True
+        mock_parser_instance = mock_parser.return_value
+        mock_parser_instance.execute.return_value = True
+
+        # Call the function with test data
+        update_airtable('Test', 'test')
+
+        # Verify that the update_by_field function was called with the correct arguments
+        mock_airtable_operations.update_by_field.assert_called_once_with('Symbol', 'Test', {'Trend': 'test'})
+
+if __name__ == '__main__':
+    unittest.main()
